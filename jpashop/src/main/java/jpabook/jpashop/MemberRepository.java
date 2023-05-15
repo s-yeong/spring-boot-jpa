617c26adcba7c68f3396872907b425ac9073e1de
s-yeong <syxxng25@gmail.com>
2023-05-16 05:17:27 +0900

    프로젝트 환경설정: JPA와 DB 설정, 동작확인

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    /**
+     * JPA를 쓰기 위해 Entity Manager가 있어야함
+     */
+
+    @PersistenceContext // 스프링 부트가 이 어노테이션이 있으면, 엔티티메니저를 주입해줌
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)    // 스프링 관련된 것을 테스트 할 거야!
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /**
+     * 엔티티 매니저를 통한 모든 데이터 변경은 항상 트랜잭션 안에서 이루어져야 한다
+     */
+
+    @Test
+    @Transactional  // @Transactional이 Test에 있으면 끝나고 Rollback 한다.
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        assertThat(findMember).isEqualTo(member);   // JPA 엔티티 동일성 보장
+        /**
+         * 같은 트랜잭션 안에서, 저장을 하고 조회를 하면 영속성 컨텍스트가 똑같다
+         * => 같은 영속성 컨택스트 안에서는 아이디 값이 같으면 같은 엔티티로 식별한다.
+         * => 그렇기 때문에 SELECT 쿼리 조차 안나감 (영속성 컨텍스트에 있네)
+         */
+    }
+}
