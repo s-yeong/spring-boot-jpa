4877197969119ec31f5455c56123785a30d0dc7e
s-yeong <syxxng25@gmail.com>
2023-05-16 23:57:18 +0900

    도메인 분석 설계: 엔티티 클래스 개발2

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,23 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    /**
+     * 값 타입은 기본적으로 변경이 불가하게 설계 해야함
+     * => 생성할 때만 값이 세팅되도록 Setter를 제공X => 변경이 불가능하게 생성자에서 값을 모두 초기화
+     */
+
+    protected Address() {
+    }
+
+    /**
+     * JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 지원하기 위해 기본 생성자가 필요 (JPA가 제약을 둠)
+     * => 보통 JPA 스펙에서는 protected까지 허용해줌
+     */
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    /**
+     * ManyToMany를 위해 JoinTable이 필요!
+     * 실제 데이터베이스에서는 중간 테이블이 존재하기 때문에
+     * 객체에서는 다대다 관계가 가능한데, 관계형 DB는 컬렉션 관계를 양쪽에서 가질 수 있는게 아니기 때문에
+     * 일대다 다대일로 풀어내는 중간 테이블이 있어야 가능함
+     *
+     * 실무에서는 거의 안씀!!!!
+     * 중간 테이블(category_item)에 컬럼을 추가할 수 없고, 세밀하게 쿼리를 실행하기 어렵기 때문에
+     * 실무에서 사용하기에는 한계가 있다 => 중간 엔티티를 만들고 @ManyToOne, @OneToMany로 매핑해서 사용하자!
+     */
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    /**
+     * 셀프로 양방향 연관관계를 검
+     */
+}
