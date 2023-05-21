d7f57051603fa7415eee2ad36b898d2a7496f7ad
s-yeong <syxxng25@gmail.com>
2023-05-21 18:28:08 +0900

    웹 계층 개발: 상품 수정

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -48,5 +50,43 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemFrom(@PathVariable Long itemId, Model model) {
+
+        // 심플하게 하기 위해서 Book만 가져온다고 가정
+        Book item = (Book) itemService.findOne(itemId);     // 캐스팅하는게 좋지는 않음..
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+        /**
+         * 누군가가 임의적으로 id를 조작해서 넘길 수 있다. -> 다른 사람의 데이터가 수정된다
+         * => 뒷단에서든 앞단에서든 유저가 아이템에 대해서 권한이 있는지에 대해 체크하는 로직이 필요하다
+         */
+
     }
 }
