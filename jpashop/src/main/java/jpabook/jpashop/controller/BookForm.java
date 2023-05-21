7f0ef6ede32dcce5bd37d2b575df8c76eb14e6fe
s-yeong <syxxng25@gmail.com>
2023-05-21 17:49:54 +0900

    웹 계층 개발: 상품 등록

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    private Long id;
+    /**
+     * 상품 수정이 있기 때문에 id가 필요하다!!
+     */
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
