49f89ede729bc48d518498441866560bd09598de
s-yeong <syxxng25@gmail.com>
2023-05-21 21:21:52 +0900

    웹 계층 개발: 주문 목록 검색, 취소

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,16 +41,33 @@ public class OrderController {
     public String order(@RequestParam Long memberId, @RequestParam Long itemId, @RequestParam int count) {
 
         orderService.order(memberId, itemId, count);
-        return "redirect:/order ";
+        return "redirect:/orders";
     }
+
     /**
      * 커멘드성 ex)주문은 컨트롤러 레벨에서는 식별자만 넘기고,
      * 서비스에서 엔티티를 찾는 것 부터 한다.
      * => 트랜잭션 안에서 엔티티를 조회해야 영속 상태로 진행이 된다.
      * (조회는 상관없음)
-     *
+     * <p>
      * 바깥에서 해버리면 트랜잭션 없이 조회했기 때문에 영속성이 없기 때문에 그 상태로 서비스에 넘기면
      * JPA와 관계없는 애가 넘어옴
      */
 
+    @GetMapping("/orders")
+    public String orderList(OrderSearch orderSearch, Model model) {
+
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+//        model.addAttribute("orderSearch", orderSearch);   // 생략
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable Long orderId) {
+
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
 }
