37c666c5cffcf797fe2930f398d2ec199a2401c2
s-yeong <syxxng25@gmail.com>
2023-05-25 17:43:28 +0900

    API 개발 고급 - 준비: 조회용 샘플 데이터 입력

---
+++ jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,106 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+/**
+ * 총 주문 2개
+ * * userA
+ *  JPA1 BOOK
+ *  JPA2 BOOK
+ * * userB
+ *  SPRING1 BOOK
+ *  SPRING2 BOOK
+ */
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        // 애플리케이션 로딩 시점에 호출
+        initService.dbInit1();
+        initService.dbInit2();
+    }
+
+    /**
+     * 스프링 라이프 사이클이 있어서 init에 Transactional 먹이고 그런게 안됨 -> 별도의 빈으로 등록함
+     */
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
+
+        public void dbInit1() {
+            // Ctrl + Alt + M
+            Member member = createMember("userA", "서울", "1", "1111");
+            em.persist(member); // -> member를 영속상태로
+
+            Book book1 = createBook("JPA1 BOOK", 10000, 100);
+            em.persist(book1);
+
+            Book book2 = createBook("JPA2 BOOK", 20000, 100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = createDelivery(member);
+
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private Delivery createDelivery(Member member) {
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());   // 실제로는 이렇게 안하지만 고객의 집 주소를 넣는다고 가정
+            return delivery;
+        }
+
+        private Book createBook(String name, int price, int stockQuantity) {
+            Book book1 = new Book();
+            book1.setName(name);
+            book1.setPrice(price);
+            // Ctrl + Alt + P
+            book1.setStockQuantity(stockQuantity);
+            return book1;
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+
+        public void dbInit2() {
+            Member member = createMember("userB", "진주", "2", "2222");
+            em.persist(member); // -> member를 영속상태로
+
+            Book book1 = createBook("SPRING1 BOOK", 20000, 200);
+            em.persist(book1);
+
+            Book book2 = createBook("SPRING2 BOOK", 40000, 300);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
+
+            Delivery delivery = createDelivery(member);
+
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+    }
+}
