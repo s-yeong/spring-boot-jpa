b2f101119be8d7ba3464bd4a0a64ce1f244dfc9e
s-yeong <syxxng25@gmail.com>
2024-01-02 20:03:02 +0900

    지연 로딩 속성 제거

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -16,7 +16,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    @OneToOne(mappedBy = "delivery")
     private Order order;
 
     @Embedded
